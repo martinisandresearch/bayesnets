fc9215f225edf9a784600e689a5c510931be29e0
Aidan Morrison <aidandmorrison@gmail,com>
2020-05-22 19:59:22 +1000

    starting unpacking hive

--- experiments/goofy/basic_hive.py
+++ experiments/goofy/basic_hive.py
@@ -5,22 +5,12 @@ Building a basic hive with varying regime and architecture parameters.
 __author__ = "Aidan Morrison <aidandmorrison@gmail.com>"
 import sys
 sys.path.append('/Users/aidanmorrison/bayesnets')
+sys.path.append('/Users/aidanmorrison/bayesnets/experiments/goofy')
 import torch
 from swarm import core, activations, io, regimes
 
 import functools
-
-def make_bee(regime, x, y, *args, **kwargs):
-    """
-    Convenience function for turning a simple arg
-    based training function into a the bee format of argless
-    This can be used to pass state between swarm iterations
-    and share resources
-    Also optional to use
-    """
-    thestrkwargs = {key: str(value) for key, value in kwargs.items()}
-    return functools.partial(regime, x, y, *args, **kwargs), thestrkwargs
-
+import utils
 
 def main():
     x = torch.linspace(-5, 5, 61)
@@ -35,7 +25,7 @@ def main():
     for momentum in (0.7, 0.9, 0.96):
         for lr in [0.002, 0.01, 0.03]:
             swarm_dict = {}
-            bee, params = make_bee(
+            bee, params = utils.make_bee(
                 regimes.default_train, x, y, hidden = 1, width = 10, num_epochs=50, activation=activations.xTanH, momentum = momentum
             )
             res = core.swarm_train(bee, num_bees=4, fields="ypred,loss", seed=seed)
--- experiments/goofy/utils.py
+++ experiments/goofy/utils.py
@@ -1,4 +1,5 @@
 import functools
+import numpy as np
 
 def make_bee(regime, x, y, *args, **kwargs):
     """
@@ -10,3 +11,36 @@ def make_bee(regime, x, y, *args, **kwargs):
     """
     thestrkwargs = {key: str(value) for key, value in kwargs.items()}
     return functools.partial(regime, x, y, *args, **kwargs), thestrkwargs
+
+def add_anindex(an_array, anindex):
+    return np.c_[an_array, np.array([anindex for a in range(len(an_array))])]
+
+def flatten_sim(data_list, loss_list, xd, yd):
+    xy = np.c_[xd, yd]
+    data_list = [add_anindex(data_list[a], a) for a in range(len(data_list))]
+    flat_data = data_list[0]
+    for i in range(1, len(data_list)):
+        flat_data = np.vstack((flat_data, data_list[i]))
+    loss_list = [add_anindex(loss_list[a], a) for a in range(len(loss_list))]
+    flat_loss = loss_list[0]
+    for i in range(1, len(loss_list)):
+        flat_loss = np.vstack((flat_loss, loss_list[i]))
+    
+    return flat_data, flat_loss, xy
+
+def unpacker(reslist, static_params):
+    data_list = [x['results']['ypred'] for x in reslist]
+    loss_list = [x['results']['loss'] for x in reslist]
+    params_list = [x['params'] for x in reslist]
+    params_list = [params_list[i].update({'index':i}) for i in range(len(params_list))]
+    xd = static_params['x']
+    yd = static_params['y']
+    all_data = []
+    all_loss = []
+    all_xy = []
+    for i in range(len(reslist)):
+        flat_data, flat_loss, xy = flatten_sim(data_list[0], loss_list[0], xd, yd)
+        flat_data = add_anindex(flat_data, i)
+        flat_loss = add_anindex(flat_loss, i)
+        xy = add_anindex(xy, i)
+    return flat_data, flat_loss, xy
