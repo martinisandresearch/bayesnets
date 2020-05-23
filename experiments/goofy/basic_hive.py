b796a286b9ed6373df9a4cc458de38e6420dd85b
Aidan Morrison <aidandmorrison@gmail,com>
2020-05-23 20:27:41 +1000

    with basic grid plots working in python and long data

--- experiments/goofy/basic_hive.py
+++ experiments/goofy/basic_hive.py
@@ -11,6 +11,9 @@ from swarm import core, activations, io, regimes
 
 import functools
 import utils
+import itertools
+from torch import nn
+import pandas as pd
 
 def main():
     x = torch.linspace(-5, 5, 61)
@@ -22,17 +25,33 @@ def main():
 
     # run experiments for each lr and save.
     reslist = []
-    for momentum in (0.7, 0.9, 0.96):
-        for lr in [0.002, 0.01, 0.03]:
-            swarm_dict = {}
-            bee, params = utils.make_bee(
-                regimes.default_train, x, y, hidden = 1, width = 10, num_epochs=50, activation=activations.xTanH, momentum = momentum
-            )
-            res = core.swarm_train(bee, num_bees=4, fields="ypred,loss", seed=seed)
-            swarm_dict['results'] = res
-            swarm_dict['params'] = params
-            reslist.append(swarm_dict.copy())
+    hidden_list = [1]
+    width_list = [10]
+    activations_list = [activations.xTanH, nn.ReLU]
+    momentum_list = [0.7, 0.9, 0.96]
+    lr_list = [0.002, 0.01, 0.03]
+    variations_list = [hidden_list, width_list, activations_list, momentum_list, lr_list]
+    param_list = itertools.product(*variations_list)
+    for params in param_list:
+        swarm_dict = {}
+        bee, params = utils.make_bee(
+            regimes.default_train, x, y, hidden = params[0], width = params[1], num_epochs=50, activation=params[2], momentum = params[3], lr = params[4]
+        )
+        res = core.swarm_train(bee, num_bees=10, fields="ypred,loss", seed=seed)
+        swarm_dict['results'] = res
+        swarm_dict['params'] = params
+        reslist.append(swarm_dict.copy())
     return reslist, static_params
+
+def get_long_results():
+    reslist, static_params = main()
+    data,loss, xy, params = utils.unpacker(reslist, static_params)
+    data_df, loss_df, param_df = utils.make_frames(data, loss, static_params, params)
+    long_data = pd.melt(data_df,id_vars = ['swarm', 'bee', 'epoch'], var_name = 'x', value_name = 'ypred')
+    long_data['x'] = long_data['x'].astype(float)
+    long_data = long_data.merge(param_df, how = 'left', left_on = 'swarm', right_on = 'swarm')
+    long_data = long_data.merge(loss_df, how = 'left', left_on = ['swarm', 'bee', 'epoch'], right_on = ['swarm', 'bee', 'epoch'])
+    return long_data
 
 if __name__ == '__main__':
     main()
--- experiments/goofy/utils.py
+++ experiments/goofy/utils.py
@@ -1,5 +1,6 @@
 import functools
 import numpy as np
+import pandas as pd
 
 def make_bee(regime, x, y, *args, **kwargs):
     """
@@ -32,15 +33,39 @@ def unpacker(reslist, static_params):
     data_list = [x['results']['ypred'] for x in reslist]
     loss_list = [x['results']['loss'] for x in reslist]
     params_list = [x['params'] for x in reslist]
-    params_list = [params_list[i].update({'index':i}) for i in range(len(params_list))]
+    params_list = [params_list[i].update({'swarm':i}) for i in range(len(params_list))]
     xd = static_params['x']
     yd = static_params['y']
     all_data = []
     all_loss = []
     all_xy = []
     for i in range(len(reslist)):
-        flat_data, flat_loss, xy = flatten_sim(data_list[0], loss_list[0], xd, yd)
+        flat_data, flat_loss, xy = flatten_sim(data_list[i], loss_list[i], xd, yd)
         flat_data = add_anindex(flat_data, i)
         flat_loss = add_anindex(flat_loss, i)
         xy = add_anindex(xy, i)
-    return flat_data, flat_loss, xy
+        params = reslist[i]['params']
+        if i == 0:
+            all_data = flat_data
+            all_loss = flat_loss
+            all_xy = xy
+            all_params = [params]
+        else:
+            all_data = np.vstack([all_data, flat_data])
+            all_loss = np.vstack([all_loss, flat_loss])
+            all_xy = np.vstack([all_xy, xy])
+            all_params.append(params)
+    return all_data, all_loss, all_xy, all_params
+
+def make_frames(data, loss, static_params, params):
+    data_df = pd.DataFrame(data, columns = [str(a) for a in static_params['x']] + ['bee', 'swarm'])
+    epoch_vec = []
+    epochs = int(data_df.shape[0]/(len(data_df['bee'].unique())*len(data_df['swarm'].unique())))
+    epochs
+    for i in range((len(data_df['bee'].unique())*len(data_df['swarm'].unique()))):
+        epoch_vec = epoch_vec + list(range(epochs))
+    data_df['epoch'] = epoch_vec
+    loss_df = pd.DataFrame(loss, columns = ['loss', 'bee', 'swarm'])
+    loss_df['epoch'] = epoch_vec
+    param_df = pd.DataFrame(params)
+    return data_df, loss_df, param_df
