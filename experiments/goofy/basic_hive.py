e140781263c3d229df07cfbbc86772ee53b98abb
Aidan Morrison <aidandmorrison@gmail,com>
2020-06-02 08:48:54 +1000

    with some slightly more advanced amendments based on Varun's advice

--- experiments/goofy/basic_hive.py
+++ experiments/goofy/basic_hive.py
@@ -15,21 +15,30 @@ import itertools
 from torch import nn
 import pandas as pd
 
-def main(width_list = [10], momentum_list = [0.9], lr_list = [0.02], num_epochs = 100, num_bees = 10, seed = 10):
+def main(width_list = None, momentum_list = None, lr_list = None, num_epochs = 100, num_bees = 10, seed = 10):
+    """
+    This is a simple experiment for allowing multiple values of width, momentum and learning rate to be tested.
+    At the moment the different activations and width are not parameters, but can be varied in the function contents.
+    I've had trouble calling from Reticulate with more than one hidden layer, however it seems to work fine in Python. 
+    """
+    ### just inserting default values here, since including iterable defaults is dangerous. 
+    if width_list is None:
+        width_list = [10]
+    if momentum_list is None:
+        momentum_list = [0.9]
+    if lr_list is None:
+        lr_list = [0.02]
+    
     x = torch.linspace(-5, 5, 61)
     y = torch.sin(x)
     static_params = {'x': x.numpy(),
                    'y': y.numpy(),
                    'seed': seed}
     
-
     # run experiments for each lr and save.
     reslist = []
     hidden_list = [1]
-    #width_list = [4,10]
-    activations_list = [activations.xTanH, nn.ReLU, activations.TanH]
-    #momentum_list = [0.7, 0.96]
-    #lr_list = [0.002, 0.03]
+    activations_list = [activations.xTanH, nn.ReLU, nn.Tanh]
     variations_list = [hidden_list, width_list, activations_list, momentum_list, lr_list]
     param_list = itertools.product(*variations_list)
     for params in param_list:
@@ -43,7 +52,20 @@ def main(width_list = [10], momentum_list = [0.9], lr_list = [0.02], num_epochs 
         reslist.append(swarm_dict.copy())
     return reslist, static_params
 
-def get_long_results(width_list = [10], momentum_list = [0.9], lr_list = [0.02], num_epochs = 100, num_bees = 10, seed = 10):
+def get_long_results(width_list = None, momentum_list = None, lr_list = None, num_epochs = 100, num_bees = 10, seed = 10):
+    """
+    This is an all-in-one step that runs the above experiment, and flattens and joins it out completely into a single object.
+    It is convenient for getting the data into a single flat object, particularly for saving or loading into another language like R.
+    However, it's not ideal for memory consumption, as there are many repreated values in the final long table. 
+    """
+    
+    ### just inserting default values here, since including iterable defaults is dangerous. 
+    if width_list is None:
+        width_list = [10]
+    if momentum_list is None:
+        momenutum_list = [0.9]
+    if lr_list is None:
+        lr_list = [0.02]
     reslist, static_params = main(width_list, momentum_list, lr_list, num_epochs, num_bees, seed)
     data,loss, xy, params = utils.unpacker(reslist, static_params)
     data_df, loss_df, xy_df, param_df = utils.make_frames(data, loss, xy, static_params, params)
--- swarm/activations.py
+++ swarm/activations.py
@@ -8,7 +8,3 @@ from torch.nn import ReLU, Tanh
 class xTanH(torch.nn.Module):
     def forward(self, x: torch.Tensor):
         return x - torch.tanh(x)
-
-class TanH(torch.nn.Module):
-    def forward(self, x: torch.Tensor):
-        return torch.tanh(x)
