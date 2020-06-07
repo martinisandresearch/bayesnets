de683caa84c7777a8787ad4182a66f5a46bd901a
nayyarv <nayyarv@gmail.com>
2020-06-07 16:18:45 +1000

    Adding a second_derivative to the complexity experiments

--- experiments/complexity/autocorr.py
+++ experiments/complexity/autocorr.py
@@ -7,7 +7,7 @@ import numpy as np
 import torch
 from torch import nn, optim
 
-from swarm import networks, core, animator
+from swarm import networks, core, animator, activations
 
 log = logging.getLogger(__name__)
 
@@ -32,19 +32,40 @@ def autocorr(x: torch.Tensor, n=10):
     return num
 
 
+def diff(x: torch.Tensor) -> torch.Tensor:
+    return x[1:] - x[:-1]
+
+
+def second_deriv(ypred: torch.Tensor, mse_weight=0.01):
+    fdiff = diff(ypred)
+    sdiff = diff(fdiff)
+
+    mse = ypred.norm()
+    # ln2 = sdiff.abs().mean()
+    ln2 = sdiff.norm()
+    return mse_weight * mse - ln2
+
+
+class Sin(torch.nn.Module):
+    def forward(self, x: torch.Tensor):
+        return torch.sin(x)
+
+
 def solo_train(
     x, hidden=2, width=2, activation=nn.ReLU, num_epochs=10, lr=0.001, momentum=0.9, corr_len=10
 ):
     net = networks.flat_net(hidden_depth=hidden, width=width, activation=activation)
     optimiser = optim.SGD(net.parameters(), lr=lr, momentum=momentum)
 
-    start_loss = autocorr(net(x), corr_len)
-    loss = 0
+    mse_weight = 1
+    loss_func = lambda ypred: second_deriv(ypred, mse_weight=mse_weight)
+
     for epoch in range(num_epochs):
+        mse_weight = min(1 / (epoch + 1), 0.1)
         optimiser.zero_grad()
         ypred = net(x)
 
-        loss = autocorr(ypred, corr_len)
+        loss = loss_func(ypred)
         log.debug("e: %s, loss: %s", epoch, loss)
         if torch.isnan(loss):
             raise RuntimeError("NaN loss, poorly configured experiment")
@@ -54,8 +75,6 @@ def solo_train(
         loss.backward()
         optimiser.step()
 
-    log.debug("First loss %s v final %s", start_loss, loss)
-
 
 def main():
     x = torch.linspace(-10, 10, 100)
@@ -64,19 +83,19 @@ def main():
         "num_epochs": 200,
         "lr": 0.005,
         "momentum": 0.5,
-        "corr_len": 20,
-        "width": 2,
-        "activation": nn.ReLU,
+        "width": 50,
+        "hidden": 3,
+        "activation": activations.Tanh,
     }
     logging.info("Starting training")
     results = core.swarm_train(solo_train, beeparams, num_bees=20, fields="ypred,loss")
     print(results["loss"])
     log.info("Making animation")
     yd = np.zeros(len(x))
-    yd[0] = -2
-    yd[-1] = 2
+    yd[0] = -0.5
+    yd[-1] = 0.5
     animator.make_animation(
-        x.detach().numpy(), yd=yd, data=results["ypred"], title="Autocorr", destfile="autocorr.mp4"
+        x.detach().numpy(), yd=yd, data=results["ypred"], title="seconddervi", destfile="sd.mp4"
     )
 
 
